bfdf33dbeb275b4bfb2c2e7a13dfed3e1f9be5dc
Ari Goswami <outsource.gb@gmail.com>
2018-06-06 07:18:52 +0530

    Little structuriation

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
-import EventDashboard from '../../feature/event/EventDashboard/EventDashboard';
+import EventDashboard from './../feature/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
