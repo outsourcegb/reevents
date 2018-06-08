6c52fc85752ae923b995573c52f5fc636d5e0f27
shashi <shashi@example.com>
2018-06-08 16:10:00 +0530

    Added basic routing stuff

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import EventDashboard from './../feature/event/EventDashboard/EventDashboard';
 import NavBar from './../feature/nav/NavBar/NavBar';
-
+import EventDetailedPage from './../feature/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from './../feature/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from './../feature/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from './../feature/user/Settings/SettingsDashboard';
+import EventForm from './../feature/event/EventForm/EventForm';
+import HomePage from './../feature/home/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <Route path="/" component={EventDashboard} />
-          <Route path="/events/:id" component={EventDashboard} />
-          <Route path="/people" component={EventDashboard} />
-          <Route path="/profile/:id" component={EventDashboard} />
-          <Route path="/settings" component={EventDashboard} />
-          <Route path="/createEvent" component={EventDashboard} />
-        </Container>
-      </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route path="/(.+)" render={() => (
+          <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+              </Switch>          
+            </Container>
+          </div>
+        )} />        
+      </div>      
     );
   }
 }
 
-export default App;
+export default App; 
