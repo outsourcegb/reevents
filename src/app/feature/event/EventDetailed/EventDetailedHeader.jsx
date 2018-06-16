316c1c711c14766f513c8b24c4493102125d79b9
Ari Goswami <outsource.gb@gmail.com>
2018-06-16 18:30:58 +0530

    Basic ui of event detail page

--- src/app/feature/event/EventDetailed/EventDetailedHeader.jsx
+++ src/app/feature/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import {Segment, Image, Item, Header, Button} from 'semantic-ui-react'
 
+const eventImageStyle = {
+    filter: 'brightness(30%)'
+};
+
+const eventImageTextStyle = {
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+};
+
 const EventDetailedHeader = () => {
   return (
        <Segment.Group>
         <Segment basic attached="top" style={{ padding: '0' }}>
-          <Image src="/assets/categoryImages/drinks.jpg" fluid />
+          <Image src="/assets/categoryImages/drinks.jpg" style={eventImageStyle} fluid />
   
-          <Segment basic>
+          <Segment basic style={eventImageTextStyle}>
             <Item.Group>
               <Item>
                 <Item.Content>
